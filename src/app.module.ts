20363ecdb2695082924fd5e9ffb546dec6f94fee
Manuela Galarza <m.galarza@uniandes.edu.co>
2026-04-30 20:01:47 -0500

    parte 6

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module }              from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { AdoptionRequestsModule } from './adoption-requests/adoption-requests.module';
 import { TypeOrmModule }       from '@nestjs/typeorm';
 import { AnimalsModule }       from './animals/animals.module';
 import { LocationsModule }     from './locations/locations.module';
@@ -27,6 +28,7 @@ import { UsersModule }         from './users/users.module';
     AnimalsModule,
     LocationsModule,
     UsersModule,
+    AdoptionRequestsModule,
   ],
 })
 export class AppModule {}
